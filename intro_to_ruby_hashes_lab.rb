0c97644dbb164e4594b1c96a40bb944cdb605f8a
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-19 01:53:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,10 +36,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] == {}
-    return hash[key] = 1 
-  else 
-    return hash[key] + 1 
+  if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
   end
   hash
   
