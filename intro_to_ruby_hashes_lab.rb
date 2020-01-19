f3f652590c61c28700152561ba88ea78b29bfe0b
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-19 01:52:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,6 +38,8 @@ end
 def update_counting_hash(hash, key)
   if hash[key] == {}
     return hash[key] = 1 
+  else 
+    return 
 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
